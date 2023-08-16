806a9db81e681a1eed1bcb8d290da2055ccff6e5
Stradios <theodor.veres.85@gmail.com>
2023-08-17 00:08:18 +0200

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-# Define source and destination paths
-source_folder="$HOME/Downloads/Flat-Remix-Pop-Blue-Dark/Flat-Remix-Blue-Dark"
-destination_system="/usr/share/icons/"
-destination_user="$HOME/.icons/"
+# Define the source and destination directories
+source_dir="Downloads/Flat-Remix-Pop-Blue-Dark/Flat-Remix-Pop-Blue-Dark"
+system_icons_dir="/usr/share/icons/"
+user_icons_dir="$HOME/.icons/"
 
-# Check if the source folder exists
-if [ -d "$source_folder" ]; then
-    # Copy to system-wide icons directory
-    sudo cp -r "$source_folder" "$destination_system"
-    
-    # Copy to user-specific icons directory
-    mkdir -p "$destination_user"
-    cp -r "$source_folder" "$destination_user"
-    
-    echo "Folder copied successfully!"
-else
-    echo "Source folder not found."
+# Check if the source directory exists
+if [ ! -d "$source_dir" ]; then
+    echo "Source directory '$source_dir' not found."
+    exit 1
 fi
+
+# Check if the system icons directory exists
+if [ ! -d "$system_icons_dir" ]; then
+    echo "System icons directory '$system_icons_dir' not found."
+    exit 1
+fi
+
+# Copy the folder to system icons directory (requires sudo)
+echo "Copying to system icons directory..."
+sudo cp -r "$source_dir" "$system_icons_dir"
+
+# Check if the user icons directory exists
+if [ ! -d "$user_icons_dir" ]; then
+    echo "Creating user icons directory: $user_icons_dir"
+    mkdir -p "$user_icons_dir"
+fi
+
+# Copy the folder to user icons directory
+echo "Copying to user icons directory..."
+cp -r "$source_dir" "$user_icons_dir"
+
+echo "Icon theme 'Flat-Remix-Pop-Blue-Dark' copied to both system and user icons directories."
