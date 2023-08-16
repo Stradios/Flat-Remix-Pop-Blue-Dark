be8bb785e44ca3aff88e649835dff21a3bb2d9f1
Stradios <theodor.veres.85@gmail.com>
2023-08-16 23:55:17 +0200

    modified:   install.sh skript

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-sudo cp -r ~/Downloads/Flat-Remix-Pop-Blue-Dark/Flat-Remix-Blue-Dark ~/.icons/
-sudo cp -r ~/Downloads/Flat-Remix-Pop-Blue-Dark/Flat-Remix-Blue-Dark /usr/share/icons/
+
+# Define source and destination paths
+source_folder="$HOME/Downloads/Flat-Remix-Pop-Blue-Dark/Flat-Remix-Blue-Dark"
+destination_system="/usr/share/icons/"
+destination_user="$HOME/.icons/"
+
+# Check if the source folder exists
+if [ -d "$source_folder" ]; then
+    # Copy to system-wide icons directory
+    sudo cp -r "$source_folder" "$destination_system"
+    
+    # Copy to user-specific icons directory
+    mkdir -p "$destination_user"
+    cp -r "$source_folder" "$destination_user"
+    
+    echo "Folder copied successfully!"
+else
+    echo "Source folder not found."
+fi
